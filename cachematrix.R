df617ff42bdc646e139431be22afd4f2d28ede31
Yunarso Anang <yunarso@gmail.com>
2021-01-17 00:38:11 +0700

    Modified the name of functions
    Modified the name of functions from "getsolve" and "setsolve" to "getInverse" and "setInverse".

--- cachematrix.R
+++ cachematrix.R
@@ -13,28 +13,28 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## This function computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then the cachesolve should retrieve the inverse 
+## matrix has not changed), then the cacheSolve should retrieve the inverse 
 ## from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getsolve()
+  m <- x$getInverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
   m <- solve(data, ...)
-  x$setsolve(m)
+  x$setInverse(m)
   m
 }
